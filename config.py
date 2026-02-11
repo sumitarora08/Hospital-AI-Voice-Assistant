6199137c3df5200a8ac4e8f431e151ec461d99aa
sumitarora08 <145918356+sumitarora08@users.noreply.github.com>
2026-02-11 15:18:37 +0530

    Modify Twilio and Gemini API keys in config.py
    Updated sensitive configuration values and added comments indicating that the keys are not real.

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-TWILIO_SID = "AC158deaaf864ea0b10f29e04214a68b49"
-TWILIO_TOKEN = "0940356c9cccb516173eb1e3ffdf58d6"
-TWILIO_NUMBER = "+18312082441"
-YOUR_NUMBER = "+919997029429"
-GEMINI_API_KEY = "AIzaSyAFp18gbkdweKShf3gzU3FoLkI0XAbsFfw"
+TWILIO_SID = "AC158deaaf864ea0b10f29e04214a68b"
+TWILIO_TOKEN = "0940356c9cccb516173eb1e3ffdf58"
+TWILIO_NUMBER = "#number you got"
+YOUR_NUMBER = "#your own number"
+GEMINI_API_KEY = "AIzaSyAFp18gdweKShf3gzU3FoLkI0XAbsF"
+
+#these keys are not real , i have edited them
+
