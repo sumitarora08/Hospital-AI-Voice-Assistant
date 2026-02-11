2289745e08bd3ab453c8778dd84326cfca694748
sumitarora08 <145918356+sumitarora08@users.noreply.github.com>
2026-02-11 20:41:00 +0530

    security concern

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-TWILIO_SID = "AC158deaaf864ea0b10f29e04214a68b"
-TWILIO_TOKEN = "0940356c9cccb516173eb1e3ffdf58"
+TWILIO_SID = ""
+TWILIO_TOKEN = ""
 TWILIO_NUMBER = "#number you got"
 YOUR_NUMBER = "#your own number"
-GEMINI_API_KEY = "AIzaSyAFp18gdweKShf3gzU3FoLkI0XAbsF"
+GEMINI_API_KEY = ""
 
 #these keys are not real , i have edited them
+
 
